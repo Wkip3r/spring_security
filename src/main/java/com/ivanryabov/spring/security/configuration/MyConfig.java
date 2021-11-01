1bd4ab6bae3ffaa8f0b9c0c53dd1bea6dcdc59df
Wkip3r <57916309+Wkip3r@users.noreply.github.com>
2021-11-01 19:24:27 +0300

    Initial commit

---
+++ src/main/java/com/ivanryabov/spring/security/configuration/MyConfig.java
@@ -0,0 +1,46 @@
+package com.ivanryabov.spring.security.configuration;
+
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+@Configuration
+@ComponentScan(basePackages = "com.ivanryabov.spring.security")
+@EnableWebMvc
+public class MyConfig {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver =
+                new InternalResourceViewResolver();
+
+        internalResourceViewResolver.setPrefix("/WEB-INF/view/");
+        internalResourceViewResolver.setSuffix(".jsp");
+
+        return internalResourceViewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+        try {
+            dataSource.setDriverClass("org.postgresql.Driver");
+            dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
+            dataSource.setUser("postgres");
+            dataSource.setPassword("postgres");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+
+        return dataSource;
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/view_for_managers.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: a18512683
+  Date: 01.11.2021
+  Time: 11:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<h3>Here you can see all employee perfomance</h3>
+
+</body>
+</html>
